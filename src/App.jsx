7b07d2ced72a33241e84cd439248c0dbe80619f3
Christoph <c.wagner.germany@gmail.com>
2024-08-05 20:52:52 +0200

    css import4

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ function App() {
           <h2>Examples</h2>
           <menu>
             <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>State</TabButton>
+            <TabButton>Components</TabButton>
           </menu>
         </section>
       </main>
