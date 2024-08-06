32d47d6ea37cd43d9f5526f4653643a445d1752b
Christoph <c.wagner.germany@gmail.com>
2024-08-06 19:24:01 +0200

    onclick5

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,43 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
             <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
         </section>
       </main>
+    </div>
+  );
+}
+
+// export default App;
+
+export const user = {
+  name: '',
+};
+
+function App() {
+  // Your goal: This function should be called WITH A VALUE for name when the <button> is clicked
+  function handleCreateUser(name) {
+    user.name = name;
+  }
+
+  return (
+    <div id="app">
+      <h1>User Login</h1>
+      <p>
+        <label>Name</label>
+        {/* You don't need to do anything with this input! You'll learn how to handle user input later */}
+        <input type="text" />
+      </p>
+
+      <p id="actions">
+        <button onClick={() => handleCreateUser('Hansi')}>Create User</button>
+      </p>
     </div>
   );
 }
