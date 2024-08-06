164a5c351b48d6de8b934d9c29ff128330f59e7f
Christoph <c.wagner.germany@gmail.com>
2024-08-06 21:15:33 +0200

    useState3

--- src/App.jsx
+++ src/App.jsx
@@ -41,4 +41,26 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
+
+// IMPORTANT: You CAN'T import & use useState like this in this Udemy environment
+// import { useState } from 'react'
+// Instead, import & use it like this:
+// import React from 'react';
+// React.useState();
+import React from 'react';
+
+export default function App() {
+    const [ selectedTopic, setSelectedTopic ] = React.useState('$100');
+    
+    function applyDiscount() {
+        setSelectedTopic('$75');
+    }
+    
+    return (
+        <div>
+            <p data-testid="price">{selectedTopic}</p>
+            <button onClick={() => applyDiscount()}>Apply Discount</button>
+        </div>
+    );
+}
