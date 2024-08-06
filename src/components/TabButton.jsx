80702a52d162d7ba3069a1e128a0ab8ff5ae8fc3
Christoph <c.wagner.germany@gmail.com>
2024-08-06 18:39:56 +0200

    onclick2

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,12 @@
 export default function TabButton({ children }) {
   // inner function (only callable from inside the parent function)
-  function handleClick() {}
+  function handleClick() {
+    console.log('Hello World');
+  }
 
   return (
     <li>
+        {/* parentheses are optional: handleClick() */}
       <button onClick={handleClick}>{children}</button>
     </li>
   );
